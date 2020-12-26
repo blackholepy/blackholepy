df60be3360b814f3e8c88790c9381addab9bd477
Chi-kwan Chan <chanc@arizona.edu>
2020-12-25 23:48:51 -0700

    Add a dummy pytest test

---
+++ tests/test_sample.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2020 Chi-kwan Chan
+# Copyright (C) 2020 Steward Observatory
+#
+# This file is part of `blackholepy`.
+#
+# `Blackholepy` is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# `Blackholepy` is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with `blackholepy`.  If not, see <http://www.gnu.org/licenses/>.
+
+def inc(x):
+    return x + 1
+
+def test_inc():
+    assert inc(3) == 4
