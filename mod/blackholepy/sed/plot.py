d196763ca2a35d88093af350c173ac24c73a74a7
Chi-kwan Chan <chanc@arizona.edu>
2020-12-26 22:42:50 -0700

    Properly align the step plot and bin boundaries for the SED

--- mod/blackholepy/sed/plot.py
+++ mod/blackholepy/sed/plot.py
@@ -40,3 +40,18 @@ def interval(avg, std, sigma=1):
     units = avg / mu
 
     return lower * units, upper * units
+
+def step(ax, nu, avg, std=None, sigma=1,
+         shade=True, ylog=True, **kwargs):
+    p = ax.step(nu, avg, where='mid', **kwargs)
+
+    if std is not None and shade:
+        l, u = interval(avg, std, sigma=sigma)
+        ax.fill_between(nu, l, u, step='mid',
+                        color=p[0].get_color(), alpha=1/3, linewidth=0)
+
+    # x-axis must be in log scale; otherwise the bin boundaries are wrong
+    ax.set_xscale('log')
+    # optionally we may set y-axis to log sacle
+    if ylog:
+        ax.set_yscale('log')
